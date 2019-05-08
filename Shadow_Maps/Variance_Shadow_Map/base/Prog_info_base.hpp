f20bfe0ecc217d73008e9851e65b1106b60dddfb
jeb1117 <30914289+jeb1117@users.noreply.github.com>
2019-05-07 23:50:55 -0600

    Add files via upload

---
+++ Shadow_Maps/Variance_Shadow_Map/base/Prog_info_base.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <vulkan/vulkan.hpp>
+#include <string>
+
+namespace base
+{
+class Prog_info_base
+{
+public:
+    bool resize_flag{false};
+    //    bool quit{false};
+
+    bool pause{false};
+
+    virtual uint32_t width() const=0;
+    virtual uint32_t height() const=0;
+    virtual const std::string& prog_name() const=0;
+
+    virtual void on_resize(uint32_t width, uint32_t height)=0;
+
+protected:
+};
+} // namespace base
